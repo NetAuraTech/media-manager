71994f59c939f01d5cd2c7779ee4a362864988ef
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-10-24 02:05:37 +0200

    feat(seo)

--- src/resources/views/content/meta.blade.php
+++ src/resources/views/content/meta.blade.php
@@ -1,15 +1,27 @@
 <?php
-    $content= $content ?? null;
-    $openGraphLogo = $openGraphLogo ?? null;
+    $defaultImage = $openGraphLogo ?? (object)['id' => '', 'width' => '', 'height' => '', 'alt' => ''];
 
-    $image = $openGraphLogo;
+    $image = image_url($defaultImage->id);
+    $width = $defaultImage->width;
+    $height = $defaultImage->height;
 
-    if($content && $content->media_id) {
-        $image = image_url($content->media_id);
+    $alt = method_exists($defaultImage, 'getDefaultAlt') ? $defaultImage->getDefaultAlt() : $defaultImage->alt;
+
+    $contentMedia = $content->media ?? null;
+
+    if ($contentMedia) {
+        $image = image_url($contentMedia->id ?? '');
+        $width = $contentMedia->width ?? '';
+        $height = $contentMedia->height ?? '';
+        $alt = $contentMedia->getDefaultAlt() ?? '';
     }
 ?>
 
 @if($image)
     <meta property='og:image' content="{{ $image }}"/>
     <meta name='twitter:image' content="{{ $image }}"/>
+    <meta property="og:image:width" content="{{ $width }}"/>
+    <meta property="og:image:height" content="{{ $height }}"/>
+    <meta property="og:image:alt" content="{{ $alt }}"/>
+    <meta property="og:image:type" content="image/webp"/>
 @endif
